422619c281a213b658d78bd371ce9fbccb80bd73
Xavi Montero <xmontero@dsitelecom.com>
2015-12-31 15:21:54 +0100

    Checks for the constructor parameter to be an array of 19 integers.

--- src/AppBundle/Model/Chromosome.php
+++ src/AppBundle/Model/Chromosome.php
@@ -6,6 +6,22 @@ class Chromosome
 {
 	function __construct( $positions )
 	{
-		//
+		if( ! is_array ( $positions ) )
+		{
+			throw new \InvalidArgumentException( "Constructor of Chromosome should be an array (of 19 elements, all being integers)." );
+		}
+
+		if( count( $positions ) != 19 )
+		{
+			throw new \InvalidArgumentException( "Constructor of Chromosome should be an array of 19 elements (all being integers)." );
+		}
+
+		foreach( $positions as $allele )
+		{
+			if( ! is_int( $allele ) )
+			{
+				throw new \InvalidArgumentException( "Constructor of Chromosome should be an array of 19 elements, all being integers." );
+			}
+		}
 	}
 }
--- src/AppBundle/Tests/Model/ChromosomeTest.php
+++ src/AppBundle/Tests/Model/ChromosomeTest.php
@@ -8,7 +8,7 @@ class ChromosomeTest extends \PHPUnit_Framework_TestCase
 {
 	public function testNew()
 	{
-		$positions = array( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 19 );
+		$positions = array( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 );
 		$sut = new Chromosome( $positions );
 		$this->assertInstanceOf( 'AppBundle\Model\Chromosome', $sut );
 	}
@@ -19,6 +19,24 @@ class ChromosomeTest extends \PHPUnit_Framework_TestCase
 	public function testNewFailsIfMissingParameter()
 	{
 		$sut = new Chromosome();
-		$this->assertInstanceOf( 'AppBundle\Model\Chromosome', $sut );
+	}
+
+	/**
+	 * @dataProvider newFailsIfParameterIsOfInvalidTypeProvider
+	 */
+	public function testNewFailsIfParameterIsOfInvalidType( $positions )
+	{
+		$this->setExpectedException( 'InvalidArgumentException' );
+		$sut = new Chromosome( $positions );
+	}
+
+	public function newFailsIfParameterIsOfInvalidTypeProvider()
+	{
+		return array
+		(
+			array( 33 ),																			// Constructor parameter should be an array, not an integer.
+			array( array( 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ) ),							// Constructor parameter should be an array, and have 19 elements.
+			array( array( 1, 2, 3, 4, 5, 6, 7, 8, 9.0, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 ) ),	// Constructor parameter should be an array, and have 19 elements, all integers.
+		);
 	}
 }
